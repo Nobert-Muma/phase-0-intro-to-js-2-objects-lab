d540aca3346c78687dc360a08adf605a1a279e28
Nobert-Muma <mumanobert17@gmail.com>
2024-06-28 13:59:04 +0300

    Working with objects in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee={
+    name:"Nobert Muma",
+    streetAddress:"15375 Mbagathi"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    for (const key in employee) {
+        delete employee[key];
+    }
+    const copyOfEmployee={...employee};
+    copyOfEmployee.name='Sam';
+    copyOfEmployee.streetAddress='11 Broadway';
+    return copyOfEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    for (const key in employee) {
+        delete employee[key];
+    }
+    employee.name="Sam";
+    employee.streetAddress="12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;   
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
